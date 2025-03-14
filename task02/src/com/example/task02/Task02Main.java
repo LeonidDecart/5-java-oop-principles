a9c06ef004ed2ef5dfe7209ee129ba70714933f5
lenadekart <decartfamily@vk.com>
2025-03-14 20:29:56 +0500

    Симоненко Леонид. Задачи 5.1 - 5.4.

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,21 +1,24 @@
 package com.example.task02;
 
 public class Task02Main {
-
     private static final Item ITEM1 = new Item("Товар 1", 10);
     private static final Item ITEM2 = new Item("Товар 2", 20);
     private static final Item ITEM3 = new Item("Товар 3", 30);
-    private static final Item ITEM4 = new Item("Товар 4", 40);
-    private static final Item ITEM5 = new Item("Товар 5", 50);
-    private static final Item ITEM6 = new Item("Товар 6", 60);
 
     public static void main(String[] args) {
         Bill bill = new Bill();
         bill.add(ITEM1, 10);
-        bill.add(ITEM3, 3);
-        bill.add(ITEM6, 1);
-        System.out.println(bill);
-        bill.add(ITEM3, 3);
-        System.out.println(bill);
+        bill.add(ITEM2, 5);
+        bill.add(ITEM3, 2);
+        System.out.println("Базовый счет:\n\n" + bill);
+
+        DiscountBill discountBill = new DiscountBill(10);
+        discountBill.add(ITEM1, 10);
+        discountBill.add(ITEM2, 5);
+        discountBill.add(ITEM3, 2);
+        System.out.println("\nСчет со скидкой:\n" + discountBill);
+        System.out.println("\nРазмер скидки: " + discountBill.getDiscount() + "%");
+        System.out.println("Абсолютное значение скидки: " + discountBill.getDiscountAmount());
+        System.out.println("Итоговая стоимость: " + discountBill.getPrice());
     }
 }
